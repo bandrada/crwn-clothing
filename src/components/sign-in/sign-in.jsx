dfec2a54d2e6d8a6733045aaddee62b898100a4c
Blaine Andrada <bandrada0@icloud.com>
2023-01-14 21:33:52 -0600

    Add Firebase redirect example

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,14 +1,26 @@
-import { 
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
+import {
+    // auth, 
     signInWithGooglePopup,
-    createUserDocFromAuth
+    createUserDocFromAuth,
+    // signInWithGoogleRedirect
  } from '../../authentication/firebase';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocFromAuth(user);
+    };
 
-    }
+    // useEffect(async () => {
+    //     const response = await getRedirectResult(auth);
+
+    //     if(response) {
+    //         const userDocRef = await createUserDocFromAuth(response.user);
+    //     }
+    // }, []);
+
     return (
         <div>
             <h1>Sign In</h1>
