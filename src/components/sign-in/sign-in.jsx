5ae0b4f60406563f7d5f653c57bb40316f141571
Blaine Andrada <bandrada0@icloud.com>
2023-01-14 21:11:50 -0600

    Add nav bar and Firebase Authentication

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,22 @@
+import { 
+    signInWithGooglePopup,
+    createUserDocFromAuth
+ } from '../../authentication/firebase';
+
+const SignIn = () => {
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocFromAuth(user);
+
+    }
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <button onClick={logGoogleUser}>
+                Sign in with Google
+            </button>
+        </div>
+    )
+}
+
+export default SignIn
