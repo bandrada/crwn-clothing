a460fa254290b44db20a168ab2e6c8147af9491a
bandrada.dev <bandrada.dev@gmail.com>
2023-12-25 17:22:24 -0500

    Simplify shop usage

--- src/App.js
+++ src/App.js
@@ -6,16 +6,12 @@ import AuthPage from './routes/auth-page';
 import { Routes, Route } from 'react-router-dom';
 import Shop from './routes/shop';
 
-const ShopPage = () => {
-  return <Shop />
-}
-
 function App() {
   return (
     <Routes>
       <Route path='/' element={<NavBar />}>
         <Route index element={<HomePage />}/>
-        <Route path='shop' element={<ShopPage />}/>
+        <Route path='shop' element={<Shop />}/>
         <Route path='auth' element={<AuthPage />}/>
       </Route>
     </Routes>
