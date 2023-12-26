19c2fe642c7a359f3cc0ecefdb560b29d94dec5e
bandrada.dev <bandrada.dev@gmail.com>
2023-12-25 19:14:02 -0500

    Feat: add product card

--- src/routes/shop.jsx
+++ src/routes/shop.jsx
@@ -1,16 +1,15 @@
 import { useContext } from 'react';
 
 import { ProductContext } from '../contexts/product';
+import ProductCard from '../components/product-card/product-card';
 
 const Shop = () => {
     const { products } = useContext(ProductContext);
 
     return (
         <div>
-            {products.map(({ id, name }) => (
-                <div key={id}>
-                    <h1>{name}</h1>
-                </div>
+            {products.map(({ product }) => (
+                <ProductCard key={product.id} product={product}/>
             ))}
         </div>
     );
