f48b2b6d842eefdfe8372d9260936af439b72d20
bandrada.dev <bandrada.dev@gmail.com>
2023-12-26 08:43:50 -0500

    Fix: return cart dropdown component

--- src/components/cart-dropdown.jsx
+++ src/components/cart-dropdown.jsx
@@ -3,10 +3,12 @@ import './cart-dropdown.scss';
 import Button from './button';
 
 const CartDropdown = () => {
-    <div className='cart-dropdown-container'>
-        <div className='cart-items' />
-        <Button>GO TO CHECKOUT</Button>
-    </div>
-}
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='cart-items' />
+            <Button>GO TO CHECKOUT</Button>
+        </div>
+    );
+};
 
 export default CartDropdown;
