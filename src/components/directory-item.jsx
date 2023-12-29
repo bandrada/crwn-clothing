bf676255461aae43eec1865d4713c1e49e8669de
bandrada.dev <bandrada.dev@gmail.com>
2023-12-29 15:28:24 -0500

    Fix: correct classname

--- src/components/directory-item.jsx
+++ src/components/directory-item.jsx
@@ -3,14 +3,14 @@ import './directory-item.scss';
 const DirectoryItem = ({item}) => {
   const { imageUrl, title } = item;
   return (
-    <div className='directory-container'>
+    <div className='directory-item-container'>
       <div
         className='background-image'
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className='directory-item-body'>
+      <div className='body'>
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
