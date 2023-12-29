fd1800995d95197928d2c16197e66ef0fb31e4f1
bandrada.dev <bandrada.dev@gmail.com>
2023-12-29 08:34:52 -0500

    Feat: add database entries

--- src/contexts/product.jsx
+++ src/contexts/product.jsx
@@ -1,5 +1,8 @@
 import { createContext, useState, useEffect } from 'react';
-import PRODUCTS from '../shop-data.json';
+
+import { addCollectionAndDocuments } from '../authentication/firebase.js';
+
+import SHOP_DATA from '../shop-data.js';
 
 // actual value to access
 export const ProductContext = createContext({
@@ -7,7 +10,10 @@ export const ProductContext = createContext({
 });
 
 export const ProductProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // }, []);
     const value = { products };
     return <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
 }
